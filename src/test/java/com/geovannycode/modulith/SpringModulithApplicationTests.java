5b980e21958644b72e299cfafa1f93b2ffab083e
Geovanny Mendoza <mgeovanny@uninorte.edu.co>
2024-10-16 23:40:12 -0500

    First commit

---
+++ src/test/java/com/geovannycode/modulith/SpringModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geovannycode.modulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringModulithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
